c28b1aae58c358a1675d09c24739705024ed2c11
Lushegtech <97428249+Lushegtech@users.noreply.github.com>
2024-11-18 22:06:03 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'], // Ensure the paths match your directory structure
   theme: {
     extend: {
       colors: {
@@ -33,4 +33,4 @@ export default {
     },
   },
   plugins: [],
-}
+};
